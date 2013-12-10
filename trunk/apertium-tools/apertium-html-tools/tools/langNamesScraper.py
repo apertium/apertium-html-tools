eab17cdbfb0ff64fe0d6c99e325b2256c446d50a
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-12-10 12:40:57 +0000

    Improve Unicode.org language name scraper and APY localized language function

--- trunk/apertium-tools/apertium-html-tools/tools/langNamesScraper.py
+++ trunk/apertium-tools/apertium-html-tools/tools/langNamesScraper.py
@@ -7,20 +7,22 @@ if __name__ == '__main__':
     parser.add_argument('-db', '--database', help='name of database file', default='unicode.db')
     args = parser.parse_args()
 
-    os.path.exists(args.database) and os.remove(args.database)
     conn = sqlite3.connect(args.database)
     c = conn.cursor()
-    c.execute('''create table languageNames (lg text, inLg text, name text)''')
-
+    c.execute('''create table if not exists languageNames (id integer primary key, lg text, inLg text, name text, unique(lg, inLg) on conflict replace)''')
     for inLang in args.languages:
-        tree = etree.parse('http://www.unicode.org/repos/cldr/tags/latest/common/main/%s.xml' % inLang)
-        languages = tree.xpath('//language')
-        i = 0
-        for language in languages:
-            if language.text:
-                c.execute('insert into languageNames values (?, ?, ?)', (inLang, language.get('type'), language.text))
-                i += 1
-        print('Added %s language names for %s' % (i, inLang))
+        try:
+            tree = etree.parse('http://www.unicode.org/repos/cldr/tags/latest/common/main/%s.xml' % inLang)
+            languages = tree.xpath('//language')
+            i = 0
+            for language in languages:
+                if language.text:
+                    c.execute('insert into languageNames values (?, ?, ?, ?)', (None, inLang, language.get('type'), language.text))
+                    i += 1
+            print('Added %s language names for %s' % (i, inLang))
+        except OSError:
+            print('Failed to retreive language %s' % inLang)
+        
     conn.commit()
     c.close()
     
