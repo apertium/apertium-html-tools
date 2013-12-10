82089494cf009b26278e3dac7da9a1ddbe16a266
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-12-10 22:03:46 +0000

    Support ISO 639-2 language codes in localized language name scraper by converting to ISO 639-1

--- trunk/apertium-tools/apertium-html-tools/tools/langNamesScraper.py
+++ trunk/apertium-tools/apertium-html-tools/tools/langNamesScraper.py
@@ -1,28 +1,43 @@
 import sqlite3, argparse, os
 from lxml import etree
 
+iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu"}
+'''
+    Scraped from https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes using
+        out = {};
+        $.each($('tr', $('table').get(1)), function(i, elem) { var rows = $('td', elem); out[$(rows[5]).text()] = $(rows[4]).text(); });
+        JSON.stringify(out);
+'''
+
+def convertISOCode(code):
+    if code in iso639Codes:
+        return (code, iso639Codes[code])
+    else:
+        return (code, code)
+
+def populateDatabase(args):
+    conn = sqlite3.connect(args.database)
+    c = conn.cursor()
+    c.execute('''create table if not exists languageNames (id integer primary key, lg text, inLg text, name text, unique(lg, inLg) on conflict replace)''')
+    for locale in args.languages:
+        locale = convertISOCode(locale)
+        try:
+            tree = etree.parse('http://www.unicode.org/repos/cldr/tags/latest/common/main/%s.xml' % locale[1])
+            languages = tree.xpath('//language')
+            changes = conn.total_changes
+            for language in languages:
+                if language.text:
+                    c.execute('''insert into languageNames values (?, ?, ?, ?)''', (None, locale[1], language.get('type'), language.text))
+            print('Added %s localized language names for %s' % (conn.total_changes - changes, locale[1] if locale[0] == locale[1] else '%s -> %s' % locale))
+        except OSError:
+            print('Failed to retreive language %s' % locale[1])
+        
+    conn.commit()
+    c.close()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scrape Unicode.org for language names in different locales.')
     parser.add_argument('languages', nargs='+', help='list of languages to add to DB')
     parser.add_argument('-db', '--database', help='name of database file', default='unicode.db')
     args = parser.parse_args()
-
-    conn = sqlite3.connect(args.database)
-    c = conn.cursor()
-    c.execute('''create table if not exists languageNames (id integer primary key, lg text, inLg text, name text, unique(lg, inLg) on conflict replace)''')
-    for inLang in args.languages:
-        try:
-            tree = etree.parse('http://www.unicode.org/repos/cldr/tags/latest/common/main/%s.xml' % inLang)
-            languages = tree.xpath('//language')
-            i = 0
-            for language in languages:
-                if language.text:
-                    c.execute('insert into languageNames values (?, ?, ?, ?)', (None, inLang, language.get('type'), language.text))
-                    i += 1
-            print('Added %s language names for %s' % (i, inLang))
-        except OSError:
-            print('Failed to retreive language %s' % inLang)
-        
-    conn.commit()
-    c.close()
-    
+    populateDatabase(args)
