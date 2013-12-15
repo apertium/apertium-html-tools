ee4636f10a5b91638914a817a7c2e99a87366dd8
Sushain Cherivirala <sushain@skc.name>
2013-12-15 03:38:06 +0000

    Add support for only saving Apertium languages from CLDR to database

--- tools/langNamesScraper.py
+++ tools/langNamesScraper.py
@@ -1,6 +1,7 @@
-import sqlite3, argparse, os
+import sqlite3, argparse, os, subprocess, re
 from lxml import etree
 
+apertiumLanguages = Set()
 iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu"}
 '''
     Scraped from https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes using
@@ -8,6 +9,20 @@ iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am"
         $.each($('tr', $('table').get(1)), function(i, elem) { var rows = $('td', elem); out[$(rows[5]).text()] = $(rows[4]).text(); });
         JSON.stringify(out);
 '''
+
+def getApertiumLanguages():
+    dirs = [('incubator', r'apertium-(\w{2,3})(?:-(\w{2,3}))?'), 
+            ('nursery', r'apertium-(\w{2,3})(?:-(\w{2,3}))?'), 
+            ('staging', r'apertium-(\w{2,3})(?:-(\w{2,3}))?'), 
+            ('trunk', r'apertium-(\w{2,3})-(?:\w{2,3})'), 
+            ('languages', r'apertium-(\w{3})'), 
+           ]
+    for (dirPath, dirRegex) in dirs:
+        svn_data = str(subprocess.check_output('svn list --xml https://svn.code.sf.net/p/apertium/svn/%s/' % dirPath, stderr=subprocess.STDOUT, shell=True), 'utf-8')
+        for langCodes in re.findall(svn_data, dirRegex, re.DOTALL):
+            apertiumLanguages.update(langCodes)
+            
+    print('Scraped %s apertium languages' % len(apertiumLanguages))
 
 def convertISOCode(code):
     if code in iso639Codes:
@@ -27,8 +42,9 @@ def populateDatabase(args):
             changes = conn.total_changes
             for language in languages:
                 if language.text:
-                    c.execute('''insert into languageNames values (?, ?, ?, ?)''', (None, locale[1], language.get('type'), language.text))
-            print('Added %s localized language names for %s' % (conn.total_changes - changes, locale[1] if locale[0] == locale[1] else '%s -> %s' % locale))
+                    if not args.apertiumOnly or (args.apertiumOnly and locale[1] in apertiumLanguages):
+                        c.execute('''insert into languageNames values (?, ?, ?, ?)''', (None, locale[1], language.get('type'), language.text))
+            print('Scraped %s localized language names for %s' % (conn.total_changes - changes, locale[1] if locale[0] == locale[1] else '%s -> %s' % locale))
         except OSError:
             print('Failed to retreive language %s' % locale[1])
         
@@ -39,5 +55,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scrape Unicode.org for language names in different locales.')
     parser.add_argument('languages', nargs='+', help='list of languages to add to DB')
     parser.add_argument('-db', '--database', help='name of database file', default='unicode.db')
+    parser.add_argument('-apOnly', '--apertiumOnly', help='only save apertium languages to database', action='store_true', default=False)
     args = parser.parse_args()
+    
+    if args.apertiumOnly:
+        getApertiumLanguages()
     populateDatabase(args)
