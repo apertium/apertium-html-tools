5f3150150690fbad224a153fba4387e8d4427eb0
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-12-24 18:23:36 +0000

    Add request time display to html-tools APY sandbox Fix bug that duplicated available generators

--- trunk/apertium-tools/apertium-html-tools/assets/js/sandbox.js
+++ trunk/apertium-tools/apertium-html-tools/assets/js/sandbox.js
@@ -13,23 +13,11 @@ $(document).ready(function() {
             request();
         }
     });
-    
-    $.ajax({
-        url: APY_URL + '/list?q=generators',
-        type: 'GET',
-        success: function (data) {
-            data = formatModes(data);
-            for(var i = 0; i < data.length; i++)
-                $('#generatorMode').append($('<option></option').val(data[i][0]).text(data[i][1]));
-        },
-        dataType: 'jsonp',
-        beforeSend: ajaxSend,
-        complete: ajaxComplete
-    });
 });
 
 function request () {
     $("#morphGenOutput").animate({ opacity: 0.5 });
+    var start_time = new Date().getTime();
     $.ajax({
         url: APY_URL + $('#sandboxInput').val(),
         type: 'GET',
@@ -37,11 +25,13 @@ function request () {
             $('#sandboxOutput').empty();
             $('#sandboxOutput').text(JSON.stringify(data, undefined, 3));
             $("#sandboxOutput").animate({ opacity: 1 });
+            $('#time').text((new Date().getTime() - start_time) + ' ms');
         },
         dataType: 'jsonp',
         failure: function (xhr, textStatus, error) {
             $('#sandboxOutput').text(error);
             $("#sandboxOutput").animate({ opacity: 1 });
+            $('#time').text(new Date().getTime() - start_time);
         },
         beforeSend: ajaxSend,
         complete: ajaxComplete
