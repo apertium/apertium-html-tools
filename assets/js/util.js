92479e6069e5c86ab4e79ce5406d45d6cb70eb83
Sushain Cherivirala <sushain@skc.name>
2014-01-11 01:51:58 +0000

    Implement language detection Add compatibility for older browsers Fix glyphicon resource URL bug

--- assets/js/util.js
+++ assets/js/util.js
@@ -36,14 +36,88 @@ function formatModes (modes) {
     }
     for(var val in modes) {
         if(val.indexOf('-') === -1)
-            toReturn.push([val, getLangByCode(val, localizedLanguageNames)]);
+            toReturn.push([val, getLangByCode(val)]);
         else {
             var mode = val.split('-')[0];
             if(modesArr.indexOf(mode) === -1)
-                toReturn.push([val, getLangByCode(mode, localizedLanguageNames)]);
+                toReturn.push([val, getLangByCode(mode)]);
             else
-                toReturn.push([val, getLangByCode(mode, localizedLanguageNames) + ' (' + val + ')']);
+                toReturn.push([val, getLangByCode(mode) + ' (' + val + ')']);
         }   
     }
     return toReturn;
+}
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+      if ( this === undefined || this === null ) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
+
+      var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+
+      fromIndex = +fromIndex || 0;
+
+      if (Math.abs(fromIndex) === Infinity) {
+        fromIndex = 0;
+      }
+
+      if (fromIndex < 0) {
+        fromIndex += length;
+        if (fromIndex < 0) {
+          fromIndex = 0;
+        }
+      }
+
+      for (;fromIndex < length; fromIndex++) {
+        if (this[fromIndex] === searchElement) {
+          return fromIndex;
+        }
+      }
+
+      return -1;
+    };
+  }
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+  Object.keys = (function () {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function (obj) {
+      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [], prop, i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
 }
