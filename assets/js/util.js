3554c5fdc8df8acbd8d96b441801748dd1aec9b2
Sushain Cherivirala <sushain@skc.name>
2014-01-12 03:34:07 +0000

    Persist form fields and user choices in html-tools

--- assets/js/util.js
+++ assets/js/util.js
@@ -57,7 +57,37 @@ function onlyUnique (value, index, self) {
     return self.indexOf(value) === index;
 }
 
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+function isSubset (subset, superset) {
+  return subset.every(function(val) { return superset.indexOf(val) >= 0; });
+}
+
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+if (!Array.prototype.every)
+{
+  Array.prototype.every = function(fun /*, thisArg */)
+  {
+    'use strict';
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function')
+        throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && !fun.call(thisArg, t[i], i, t))
+        return false;
+    }
+
+    return true;
+  };
+}
+
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (searchElement, fromIndex) {
       if ( this === undefined || this === null ) {
@@ -89,7 +119,7 @@ if (!Array.prototype.indexOf) {
     };
   }
 
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 if (!Object.keys) {
   Object.keys = (function () {
     'use strict';
