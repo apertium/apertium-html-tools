7cba98f40462e08a51169dd4a426c3e104d09af0
Sushain Cherivirala <sushain@skc.name>
2014-04-07 22:03:26 +0000

    Better config (more options, seperate file) and description for search engines

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,7 +1,4 @@
-﻿var APY_URL = '//localhost:2737';
-var ALLOWED_LANGS = undefined; //Set to undefined for all languages
-
-function ajaxSend() {
+﻿function ajaxSend() {
     $('#loading-indicator').show();
 }
 
@@ -22,13 +19,26 @@ $(document).ready(function () {
     $('.navbar').css('margin-top', '0px');
     $('body > .container').css('margin-top', '0px');
 
+    if(SHOW_NAVBAR) {
+        if(ENABLED_MODES === undefined)
+            $('.nav a').removeClass('hide');
+        else
+            $.each(ENABLED_MODES, function () {
+                $('.nav a[data-mode=' + this + ']').removeClass('hide');
+            });
+    }
+    else {
+        $('.navbar-toggle').hide();
+        $('.nav').hide();
+    }
+
     var hash = parent.location.hash;
 
     if(!hash || !$(hash + 'Container')) {
-        hash = '#translation';
-		parent.location.hash = hash;
-	}
-	
+        hash = '#' + DEFAULT_MODE;
+        parent.location.hash = hash;
+    }
+
     $('.modeContainer' + hash + 'Container').show();
     $('.nav li > a[data-mode=' + hash.substring(1) + ']').parent().addClass('active');
 
@@ -61,38 +71,26 @@ $(document).ready(function () {
             $(this).attr('src', $(this).attr('data-src'));
         });
     });
-
-    var parameters = getParameters();
-    if((!('sandbox' in parameters) || parameters['sandbox'].replace('/', '') === '0') && !(hash && hash.substring(1) === 'sandbox'))
-        $('.nav a[data-mode=sandbox]').hide();
-
-    function getParameters() {
-        var searchString = window.location.search.substring(1),
-            params = searchString.split('&'),
-            hash = {};
-
-        if(searchString === '')
-            return {};
-
-        for(var i = 0; i < params.length; i++) {
-            var val = params[i].split('=');
-            hash[unescape(val[0])] = unescape(val[1]);
-        }
-        return hash;
-    }
 });
 
 function filterLangList(langs, filterFn) {
-    if(!ALLOWED_LANGS)
+    if(ALLOWED_LANGS === undefined && ALLOWED_VARIANTS === undefined)
         return langs;
     else {
         if(!filterFn)
             filterFn = function (code) {
-              return ALLOWED_LANGS.indexOf(code) !== -1 || ((code.indexOf('-') !== -1 && (ALLOWED_LANGS.indexOf(code.split('-')[0]) + ALLOWED_LANGS.indexOf(code.split('-')[1])) !== -2));  
+                return  allowedLang(code) || ((code.indexOf('-') !== -1 && (allowedLang(code.split('-')[0]) || allowedLang(code.split('-')[1]))));
             };
 
         return langs.filter(filterFn);
     }
+}
+
+function allowedLang (code) {
+    if(code.indexOf('_') === -1)
+        return ALLOWED_LANGS === undefined || ALLOWED_LANGS.indexOf(code) !== -1;
+    else
+        return allowedLang(code.split('_')[0]) && (ALLOWED_VARIANTS === undefined || ALLOWED_VARIANTS.indexOf(code.split('_')[1]) !== -1);
 }
 
 function onlyUnique(value, index, self) {
