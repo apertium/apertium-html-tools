029672d9b5105ac61c905c1bb6be142e28eb4f62
Sushain Cherivirala <sushain@skc.name>
2014-04-07 22:44:05 +0000

    oops, forgot to add config file

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+var APY_URL = '//localhost:2737';
+var ALLOWED_LANGS = undefined; //Set to undefined for all languages, [] for none
+var ALLOWED_VARIANTS = undefined; //Set to undefined for all varieties, [] for none
+var ENABLED_MODES = ['translation', 'analyzation', 'generation', 'sandbox']; //Set to undefined or ['translation', 'analyzation', 'generation', 'sandbox'] for all modes, [] for none
+var DEFAULT_MODE = 'translation';
+var SHOW_NAVBAR = true;
