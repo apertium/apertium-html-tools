2b5aecc190e674bdaf54cc9532fa88b8e7b6a30f
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-04-10 00:13:01 +0000

    Add caching JS file (oops)

---
+++ trunk/apertium-tools/apertium-html-tools/assets/js/caching.js
@@ -0,0 +1,15 @@
+function cache(name, value) {
+    if(localStorage) {
+        localStorage[name] = JSON.stringify(value);
+        localStorage[name + '_timestamp'] = Date.now();
+    }
+}
+
+function readCache(name, type) {
+    if(localStorage && localStorage[name] && localStorage[name + '_timestamp']) {
+        if(parseInt(localStorage[name + '_timestamp']) + config[type.toUpperCase() + '_CACHE_EXPIRY'] * 3600000 > Date.now())
+            return JSON.parse(localStorage[name]);
+        else
+        	return null;
+    }
+}
