481f4b8d202e7e34ad60b705f84feb8cc621655a
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-04-11 17:37:48 +0000

    move Google Analytics settings to config file

--- trunk/apertium-tools/apertium-html-tools/assets/js/config.js
+++ trunk/apertium-tools/apertium-html-tools/assets/js/config.js
@@ -11,6 +11,8 @@ var config = {
     ENABLED_MODES: ['translation', 'analyzation', 'generation', 'sandbox'],
     DEFAULT_MODE: 'translation',
     SHOW_NAVBAR: true,
+    GOOGLE_ANALYTICS_PROPERTY: undefined,
+    GOOGLE_ANALYTICS_TRACKING_ID: undefined,
     LIST_REQUEST_CACHE_EXPIRY: 24,
     LANGUAGE_NAME_CACHE_EXPIRY: 24,
     LOCALIZATION_CACHE_EXPIRY: 24,
