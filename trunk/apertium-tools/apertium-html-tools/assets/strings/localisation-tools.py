6ab400aa61caf16549b8026e82ec0c623719d0a9
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-05-05 20:04:34 +0000

    localisation helper tools

---
+++ trunk/apertium-tools/apertium-html-tools/assets/strings/localisation-tools.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from collections import OrderedDict
+import argparse, json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Manipulate localisation files')
+    parser.add_argument('action', choices=['new', 'create', 'clean', 'sort', 'sort+clean', 'clean+sort'])
+    parser.add_argument('codes', nargs='+', help='language codes for filename')
+    parser.add_argument('-u', '--unavailableString', default='%%UNAVAILABLE%%', help='placeholder value for unavailable string')
+    parser.add_argument('-c', '--canonicalFile', default='eng.json', help='base file for creation')
+    parser.add_argument('-d', '--defaultStrings', default=False, action='store_true', help='use strings from canonical file')
+    args = parser.parse_args()
+    args.actions = args.action.split('+')
+
+    with open(args.canonicalFile) as f:
+        canonicalStrings = json.loads(f.read(), object_pairs_hook=OrderedDict)
+        if not args.defaultStrings:
+            canonicalStrings = OrderedDict([(k, args.unavailableString) for k, v in canonicalStrings.items()])
+
+    for fname in args.codes:
+        fname = '{}.json'.format(fname)
+        if 'create' in args.actions or 'new' in args.actions:
+            with open(fname, 'w+') as f:
+                f.write(json.dumps(canonicalStrings, indent=4, sort_keys=False, ensure_ascii=False))
+        if 'clean' in args.actions:
+            with open(fname, 'r+') as f:
+                strings = OrderedDict(filter(lambda x: x[0] in canonicalStrings.keys(), json.loads(f.read()).items()))
+                for stringName in set(canonicalStrings.keys()) - set(strings.keys()):
+                    strings[stringName] = args.unavailableString
+                f.seek(0)
+                f.write(json.dumps(strings, indent=4, sort_keys=False, ensure_ascii=False))
+                f.truncate()
+        if 'sort' in args.actions:
+            with open(fname, 'r+') as f:
+                strings = OrderedDict(json.loads(f.read()).items())
+                strings = OrderedDict(sorted(strings.items(), key=lambda x: -1 if x[0] not in canonicalStrings.keys() else list(canonicalStrings.keys()).index(x[0])))
+                f.seek(0)
+                f.write(json.dumps(strings, indent=4, sort_keys=False, ensure_ascii=False))
+                f.truncate()
+                
