1803fb6ccda169b671acde22d44da70af62019bf
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-05-13 14:55:45 +0000

    Request /listLanguageNames if only language names required (first visit to index.[lang].html), else request [lang].json for both language names and interface strings

--- trunk/apertium-tools/apertium-html-tools/assets/js/localization.js
+++ trunk/apertium-tools/apertium-html-tools/assets/js/localization.js
@@ -1,6 +1,6 @@
 ﻿var locale;
 var languages = {'af': 'Afrikaans', 'id': 'Bahasa Indonesia', 'cy': 'Cymraeg', 'de': 'Deutsch', 'en': 'English', 'ga': 'Gaeilge', 'gv': 'Gaelg', 'gd': 'Gàidhlig', 'rn': 'Ikirundi', 'sw': 'Kiswahili', 'lg': 'Luganda', 'mt': 'Malti', 'nl': 'Nederlands', 'sq': 'Shqip', 'vi': 'Tiếng Việt', 'tr': 'Türkçe', 'az': 'azərbaycan', 'br': 'brezhoneg', 'ca': 'català', 'da': 'dansk', 'se': 'davvisámegiella', 'et': 'eesti', 'es': 'español', 'eo': 'esperanto', 'eu': 'euskara', 'fr': 'français', 'fo': 'føroyskt', 'ia': 'interlingua', 'xh': 'isiXhosa', 'zu': 'isiZulu', 'it': 'italiano', 'mfe': 'kreol morisien', 'lv': 'latviešu', 'lt': 'lietuvių', 'hu': 'magyar', 'nb': 'norsk bokmål', 'nn': 'nynorsk', 'uz': 'oʻzbekcha', 'pl': 'polski', 'pt': 'português', 'ro': 'română', 'rm': 'rumantsch', 'sk': 'slovenčina', 'sl': 'slovenščina', 'fi': 'suomi', 'sv': 'svenska', 'is': 'íslenska', 'cs': 'čeština', 'el': 'Ελληνικά', 'tg': 'Тоҷикӣ', 'ba': 'башҡортса', 'be': 'беларуская', 'bg': 'български', 'os': 'ирон', 'kum': 'къумукъча', 'ky': 'кыргызча', 'mk': 'македонски', 'ru': 'русский', 'tt': 'татарча', 'uk': 'українська', 'kk': 'қазақша', 'hy': 'հայերեն', 'he': 'עברית', 'ur': 'اردو', 'ar': 'العربية', 'fa': 'فارسی', 'ne': 'नेपाली', 'mr': 'मराठी', 'hi': 'हिंदी', 'as': 'অসমীয়া', 'bn': 'বাংলা', 'pa': 'ਪੰਜਾਬੀ', 'te': 'తెలుగు', 'ml': 'മലയാളം', 'si': 'සිංහල', 'th': 'ไทย', 'lo': 'ລາວ', 'zh': '中文', 'ko': '한국어', 'mrj': 'Мары йӹлмӹ', 'gl': 'galego', 'myv': 'Эрзянь кель', 'oc': 'occitan', 'cv': 'чӑвашла', 'arg': 'aragonés', 'ast': 'asturianu', 'msa': 'bahasa malay', 'hbs': 'srpskohrvatski'};
-var iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
+var iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"};
 var rtlLanguages = ['heb', 'ara', 'pes', 'urd'];
 var languagesInverse = {}, iso639CodesInverse = {};
 var localizedLanguageCodes = {}, localizedLanguageNames = {};
@@ -13,13 +13,6 @@ $(document).ready(function () {
     });
     $.each(iso639Codes, function (key, value) {
         iso639CodesInverse[value] = key
-    });
-
-    $('.localeSelect').change(function () {
-        locale = $(this).val();
-        sendEvent('localization', 'localize', locale);
-        localizeEverything(false);
-        persistChoices('localization');
     });
 
     var possibleItems = {'translation': getPairs, 'generation': getGenerators, 'analyzation': getAnalyzers};
@@ -40,11 +33,16 @@ $(document).ready(function () {
         persistChoices('localization');
     });
 
+    $('.localeSelect').change(function () {
+        locale = $(this).val();
+        sendEvent('localization', 'localize', locale);
+        localizeEverything(false);
+        persistChoices('localization');
+    });
+
     function localizeEverything(stringsFresh) {
-        localizeLanguageNames();
         localizeInterface();
-        if(!stringsFresh)
-            localizeStrings();
+        localizeStrings(stringsFresh);
 
         if(window.history.replaceState) {
             var urlParams = [], urlParamNames = ['dir'];
@@ -174,29 +172,34 @@ function generateLanguageList() {
     return languages;
 }
 
-function localizeLanguageNames() {
-    var languages = generateLanguageList();
-
-    var localizedNames = readCache(locale + '_names', 'LANGUAGE_NAME');
-    if(localizedNames) {
-        handleLocalizedNames(localizedNames)
-    }
+function localizeLanguageNames(localizedNamesFromJSON) {
+    if(localizedNamesFromJSON)
+        handleLocalizedNames(localizedNamesFromJSON);
     else {
-        console.error(locale + ' localized names cache ' + (localizedNames === null ? 'stale' : 'miss') + ', retrieving from server');
-        $.jsonp({
-            url: config.APY_URL + '/listLanguageNames?locale=' + locale + '&languages=' + languages.join('+'),
-            beforeSend: ajaxSend,
-            complete: ajaxComplete,
-            success: function (data) {
-                handleLocalizedNames(data);
-                cache(locale + '_names', data);
-            },
-            error: function () {
-                localizedLanguageNames = {};
-            }
-        });
-    }
+        var languages = generateLanguageList();
 
+        var localizedNames = readCache(locale + '_names', 'LANGUAGE_NAME');
+        if(localizedNames) {
+            handleLocalizedNames(localizedNames)
+            cache(locale + '_names', data);
+        }
+        else {
+            console.error(locale + ' localized names cache ' + (localizedNames === null ? 'stale' : 'miss') + ', retrieving from server');
+            $.jsonp({
+                url: config.APY_URL + '/listLanguageNames?locale=' + locale + '&languages=' + languages.join('+'),
+                beforeSend: ajaxSend,
+                complete: ajaxComplete,
+                success: function (data) {
+                    handleLocalizedNames(data);
+                    cache(locale + '_names', data);
+                },
+                error: function () {
+                    localizedLanguageNames = {};
+                }
+            });
+        }
+    }
+    
     function handleLocalizedNames(localizedNames) {
         localizedLanguageNames = localizedNames;
         localizedLanguageCodes = {};
@@ -217,26 +220,32 @@ function localizeLanguageNames() {
     }
 }
 
-function localizeStrings() {
-    var localizations = readCache(locale + '_localizations', 'LOCALIZATION');
-    if(localizations) {
-        handleLocalizations(localizations);
-    }
+function localizeStrings(stringsFresh) {
+    if(stringsFresh)
+        localizeLanguageNames();
     else {
-        console.error(locale + ' localizations cache ' + (localizations === null ? 'stale' : 'miss') + ', retrieving from server');
-        $.ajax({
-            url: './strings/' + locale + '.json',
-            type: 'GET',
-            dataType: 'text',
-            success: function (data) {
-                data = JSON.parse(data.replace(/[\n\t\r]/g, ''));
-                handleLocalizations(data);
-                cache(locale + '_localizations', data);
-            },
-            error: function (jqXHR, textStatus, errorThrow) {
-                console.error('Failed to fetch localized strings for ' + locale + ': ' + errorThrow);
-            }
-        });
+        var localizations = readCache(locale + '_localizations', 'LOCALIZATION');
+        if(localizations) {
+            handleLocalizations(localizations);
+            localizeLanguageNames(localizations['@langNames']);
+        }
+        else {
+            console.error(locale + ' localizations cache ' + (localizations === null ? 'stale' : 'miss') + ', retrieving from server');
+            $.ajax({
+                url: './strings/' + locale + '.json',
+                type: 'GET',
+                dataType: 'text',
+                success: function (data) {
+                    data = JSON.parse(data.replace(/[\n\t\r]/g, ''));
+                    handleLocalizations(data);
+                    localizeLanguageNames(data['@langNames']);
+                    cache(locale + '_localizations', data);
+                },
+                error: function (jqXHR, textStatus, errorThrow) {
+                    console.error('Failed to fetch localized strings for ' + locale + ': ' + errorThrow);
+                }
+            });
+        }
     }
 
     function handleLocalizations(localizations) {
@@ -285,6 +294,10 @@ function getLangByCode(code) {
     code = code ? code.trim() : code;
     if(localizedLanguageNames[code])
         return localizedLanguageNames[code];
+    else if(iso639Codes[code] && localizedLanguageNames[iso639Codes[code]])
+        return localizedLanguageNames[iso639Codes[code]];
+    else if(iso639CodesInverse[code] && localizedLanguageNames[iso639CodesInverse[code]])
+        return localizedLanguageNames[iso639CodesInverse[code]];
     else if(languages[code])
         return languages[code];
     else if(iso639Codes[code] && languages[iso639Codes[code]])
