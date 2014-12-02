23eb33091b3bbb17dbaab9e70282508df2882689
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-12-02 23:08:16 +0000

    Add progress wiki table script

---
+++ trunk/apertium-tools/apertium-html-tools/assets/strings/progresstable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep "completion" *.json | sort -nrk1.33,1.35 -nrk1.38,1.43 | awk -F '"' 'BEGIN {print "{| class=\"wikitable\"\n|-\n!|code\n!|CBE*\n!|CBC**" } {sub(/\..*$/, "", $1); split($4, s, " "); print "|-\n|| " $1 " || " s[1] " || " s[2]} END { print "|}\n\n&nbsp;*CBE: completion by entries <br>\n&nbsp;**CBC: completion by characters"}'
