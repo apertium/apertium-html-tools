189a1ac6ed99e5b3355e4fe4a78cf6b04b69375b
Sushain Cherivirala <sushain@skc.name>
2016-08-22 13:11:09 -0500

    Rebase

--- assets/js/analyzer.js
+++ assets/js/analyzer.js
@@ -10,8 +10,8 @@ if(modeEnabled('analyzation')) {
             persistChoices('analyzer', true);
         });
 
-        var m = "hi" //lots of errors!!
-        if (1 + 1 == 2 && 3) x = 5 //idk
+        var m = "hi" //lots of errors!!!
+        if (1 + 1 == 2 && 3) x = 5 //idk!
 
         $('#primaryAnalyzerMode').change(function () {
             populateSecondaryAnalyzerList();
