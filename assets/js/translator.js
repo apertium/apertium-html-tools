29b009748a0c085c5e770aea6bec70cea4c6224f
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2017-01-20 22:22:53 +0100

    epiphany-browser gives "" as MIME (Blob.type) regardless, accept that too
    also add accept-attribute for file-picker

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -607,7 +607,9 @@ function translateDoc() {
             docTranslateError(dynamicLocalizations['File_Too_Large'], 'File_Too_Large');
         }
         else {
+            // Keep in sync with accept attribute of input#fileInput:
             var allowedMimeTypes = [
+                '',               // epiphany-browser gives this instead of a real MIME type
                 'text/plain', 'text/html',
                 'text/rtf', 'application/rtf',
                 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
@@ -670,6 +672,7 @@ function translateDoc() {
                 sendEvent('translator', 'translateDoc', curSrcLang + '-' + curDstLang, file.size);
             }
             else {
+                console.log("Browser gave MIME type as", file.type);
                 docTranslateError(dynamicLocalizations['Format_Not_Supported'], 'Format_Not_Supported');
             }
         }
