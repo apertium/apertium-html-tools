db4b63824889a8ba67485414f6cb6d6a5fdd2913
Godhia <godhiam@amazon.com>
2017-06-07 09:03:47 +0530

    Remove custom exception

--- assets/js/store.js
+++ assets/js/store.js
@@ -62,26 +62,11 @@ Store.prototype.has = function (key/*: string*/)/*: bool*/ {
 
 Store.prototype.able = function ()/*: bool*/ {
     try {
-        throwLocalStorageNotAccessibleError();
         return !!(window.localStorage);
     }
     catch(e) {
-        if(e.name === 'localStorageNotAccessibleError') {
-            console.warn(e.message);
-            return false;
-        }
-        else {
-            console.warn('Some exception has occurred while loading the page.');
-            throw e;
-        }
+
     }
 };
-
-function throwLocalStorageNotAccessibleError() {
-    throw {
-        'name': 'localStorageNotAccessibleError',
-        'message': 'Cookies are disabled. Thus, unable to access localStorage.'
-    };
-}
 
 /*:: export {Store} */
