a11e1e7fb723a857b81615d997e0505f84eb1f2c
Godhia <godhiam@amazon.com>
2017-06-11 15:51:01 +0530

    Add support for polyfill

--- assets/js/util.js
+++ assets/js/util.js
@@ -10,6 +10,32 @@ var SPACE_KEY_CODE = 32, ENTER_KEY_CODE = 13,
 var TEXTAREA_AUTO_RESIZE_MINIMUM_WIDTH = 768,
     THRESHOLD_REQUEST_LENGTH = 2000; // keep 48 characters buffer for remaining params
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 function ajaxSend() {
     $('#loadingIndicator').show();
 }
