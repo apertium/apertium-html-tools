4fae37f841383300588a51ae9f935d69bb4dfe77
mono <monish1247@gmail.com>
2017-06-21 11:42:32 +0530

    Travis - fix indentation

--- assets/js/util.js
+++ assets/js/util.js
@@ -358,8 +358,8 @@ function displayUnobtrusiveWarning() {
     var messageDuration = 10000;
 
     $('#installationNotice').removeClass('hide').fadeIn('slow')
-                                                .delay(messageDuration)
-                                                .fadeOut('slow', hideUnobtrusiveWarning);
+        .delay(messageDuration)
+        .fadeOut('slow', hideUnobtrusiveWarning);
     $('#installationNotice .fa-times').click(function () {
         $('#installationNotice').fadeOut('fast', hideUnobtrusiveWarning);
     });
@@ -367,8 +367,8 @@ function displayUnobtrusiveWarning() {
         $(this).stop(true);
     }).mouseout(function () {
         $(this).animate()
-               .delay(messageDuration)
-               .fadeOut('slow', hideUnobtrusiveWarning);
+        .delay(messageDuration)
+       .fadeOut('slow', hideUnobtrusiveWarning);
     });
 }
 
