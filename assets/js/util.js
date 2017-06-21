93adbe24f4591d236f9f5cdc1c00e8adcdbe1754
mono <monish1247@gmail.com>
2017-06-21 19:35:02 +0530

    Travis

--- assets/js/util.js
+++ assets/js/util.js
@@ -43,18 +43,22 @@ if (typeof Object.assign != 'function') {
 
 function debounce(func, wait, immediate) {
     var timeout;
-    return function() {
+    return function () {
         var context = this, args = arguments;
-        var later = function() {
+        var later = function () {
             timeout = null;
-            if (!immediate) func.apply(context, args);
+            if(!immediate) {
+                func.apply(context, args);
+            }
         };
         var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+        if(callNow) {
+            func.apply(context, args);
+        }
     };
-};
+}
 
 function ajaxSend() {
     $('#loadingIndicator').show();
