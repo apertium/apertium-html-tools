12e039b2abacfc35d04ac505b984848da7b39d25
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2017-12-16 20:05:02 +0100

    Declare modules to Flow

---
+++ flow-typed/apertium.js
@@ -0,0 +1,39 @@
+declare module 'analyzer' {
+  declare module.exports: any;
+}
+
+declare module 'config' {
+  declare module.exports: any;
+}
+
+declare module 'generator' {
+  declare module.exports: any;
+}
+
+declare module 'init' {
+  declare module.exports: any;
+}
+
+declare module 'localization' {
+  declare module.exports: any;
+}
+
+declare module 'persistence' {
+  declare module.exports: any;
+}
+
+declare module 'sandbox' {
+  declare module.exports: any;
+}
+
+declare module 'store' {
+  declare module.exports: any;
+}
+
+declare module 'translator' {
+  declare module.exports: any;
+}
+
+declare module 'util' {
+  declare module.exports: any;
+}
