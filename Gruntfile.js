5c5815640117151d34b87810b5f60c80fe571637
Jonathan Pan <jonpan4@gmail.com>
2018-01-14 19:37:57 -0800

    Run QUnit tests on CircleCI.

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function( grunt ) {
+    grunt.initConfig( {
+        qunit: {
+            all: "build/qunit-test.html"
+        }
+    } );
+    grunt.loadNpmTasks( "grunt-contrib-qunit" );
+};
