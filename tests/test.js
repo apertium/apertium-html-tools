6d930db856d8bcdaf05f8260fa9fac0a41c12ee7
JPJPJPOPOP <jonpan4@gmail.com>
2018-01-17 19:22:24 -0800

    Fix houndci errors.

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
+/* global QUnit, config, modeEnabled */
+
 QUnit.module('Change mode');
 QUnit.test('Mode test', function (assert) {
-    assert.expect(4);
+    assert.expect(4); // eslint-disable-line no-magic-numbers
     $('a[data-mode="sandbox"]').click();
     assert.equal($('#sandboxContainer').css('display'), 'block', 'Switching to sandbox mode');
     $('a[data-mode="translation"]').click();
@@ -13,7 +15,7 @@ QUnit.test('Mode test', function (assert) {
 
 QUnit.module('Verify modeEnabled is consistent with config file');
 QUnit.test('Check modeEnabled', function (assert) {
-    assert.expect(4);
+    assert.expect(4); // eslint-disable-line no-magic-numbers
     assert.equal(config.ENABLED_MODES.includes('translation'), modeEnabled('translation'), 'Translation mode is consistent');
     assert.equal(config.ENABLED_MODES.includes('generation'), modeEnabled('generation'), 'Generation mode is consistent');
     assert.equal(config.ENABLED_MODES.includes('analyzation'), modeEnabled('analyzation'), 'Analyzation mode is consistent');
