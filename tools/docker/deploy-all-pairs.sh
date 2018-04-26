277d50f0cb32aac1e20ca402130331071e3d147e
Sushain Cherivirala <sushain@skc.name>
2018-04-26 18:03:20 -0500

    Update Dockerfile and use APy dockerfile from source

---
+++ tools/docker/deploy-all-pairs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+run_in_apy() {
+    docker-compose exec apy bash -c "$1"
+}
+
+run_in_apy "apt-get update && apt-get upgrade -y"
+run_in_apy "cd /source && wget -nv https://raw.githubusercontent.com/unhammer/apertium-get/master/apertium-get -O apertium-get && chmod +x apertium-get"
+run_in_apy "cd /source && ./apertium-get -m | grep -v '^$\|^\s*\#' | sort | xargs -n 1 -P 6 timeout 10m ./apertium-get -s"
+run_in_apy "cd /source && ./apertium-get -l | grep -v '^$\|^\s*\#' | sort | xargs -n 1 -P 6 timeout 10m ./apertium-get"
+run_in_apy "cd /root/apertium-apy && git pull"
+docker-compose restart apy html-tools
