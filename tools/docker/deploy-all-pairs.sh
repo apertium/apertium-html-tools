3d343ea22a25b8741fd6e49c5523d0388f92f76c
Sushain Cherivirala <sushain@skc.name>
2019-03-29 12:23:25 -0500

    Update deploy-all-pairs script for Git

--- tools/docker/deploy-all-pairs.sh
+++ tools/docker/deploy-all-pairs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 run_in_apy() {
-    docker-compose exec apy bash -c "$1"
+    docker-compose exec apy bash -c "cd /source && $1"
 }
 
-run_in_apy "apt-get -qq update && apt-get upgrade -qq && apt-get install -qq wget"
-run_in_apy "cd /source && wget -nv https://raw.githubusercontent.com/unhammer/apertium-get/master/apertium-get -O apertium-get && chmod +x apertium-get"
-run_in_apy "cd /source && ./apertium-get -m | grep -v '^$\\|^\\s*\\#' | sort | xargs -n 1 -P 6 timeout 10m ./apertium-get -s"
-run_in_apy "cd /source && ./apertium-get -l | grep -v '^$\\|^\\s*\\#' | sort | xargs -n 1 -P 6 timeout 10m ./apertium-get"
+run_in_apy "apt-get -qq update && apt-get upgrade -qq && apt-get install -qq curl"
+run_in_apy "curl https://raw.githubusercontent.com/apertium/apertium-get/master/apertium-get > apertium-get && chmod +x apertium-get"
+run_in_apy "./apertium-get -m | grep -v '^$\\|^\\s*\\#' | sort | xargs -n 1 -P 6 timeout 8m ./apertium-get -s -d 1"
+run_in_apy "./apertium-get -l | grep -v '^$\\|^\\s*\\#' | sort | xargs -n 1 -P 6 timeout 8m ./apertium-get -d 1"
 docker-compose pull
 docker-compose up -d --build
