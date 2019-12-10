e26bc0afbf45624e73c1e2bda890774ce06653d6
diogoscf <diogo.scf2003@gmail.com>
2019-12-10 19:20:13 +0100

    Comply with flow

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -556,8 +556,10 @@ function populateTranslationList() {
 
     var maxSrcLangsWidth, maxDstLangsWidth;
 
+    var direction = locale ? langDirection(locale) : 'ltr';
+
     // figure out how much space is actually available for the columns
-    if(langDirection(locale) === 'ltr') {
+    if(direction === 'ltr') {
         maxSrcLangsWidth = $(window).width() - $('#srcLanguagesDropdownTrigger').offset().left - TRANSLATION_LISTS_BUFFER;
         maxDstLangsWidth = $('#dstLanguagesDropdownTrigger').offset().left + $('#dstLanguagesDropdownTrigger').outerWidth() -
                             TRANSLATION_LISTS_BUFFER;
