2171911abb4d51ff0bfb516fc60dc8e5c477067b
Maathavan <maathumaathavan@gmail.com>
2019-12-15 17:45:42 +0530

    Update translator.js

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1254,8 +1254,8 @@ function setDefaultSrcLang() {
     var browserLangs = window.navigator.languages; // Chrome, Mozilla and Safari
     var ieLang = window.navigator.userlanguage || window.navigator.browserlanguage; // Internet Explorer
     var prefSrcLang;
-    $.each(browserLangs, function (i) {
-        var browserLang = getLangCode(browserLangs[i]);
+    $.each(browserLangs, function (i, lang) {
+        var browserLang = getLangCode(lang);
         if(checkLangPairAvailable(browserLang)) {
             prefSrcLang = browserLang;
             return false;
