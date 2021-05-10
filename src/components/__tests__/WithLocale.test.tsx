db0410c28d0e21d3c6ce22f276705422b1586728
Sushain Cherivirala <sushain@skc.name>
2021-05-09 19:14:44 -0700

    Full test coverage of WithLocale

--- src/components/__tests__/WithLocale.test.tsx
+++ src/components/__tests__/WithLocale.test.tsx
@@ -1,37 +1,14 @@
 import * as React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import mockAxios from 'jest-mock-axios';
 
+import Config from '../../../config';
 import { LocaleContext } from '../../context';
 import WithLocale from '../WithLocale';
 
-const originalLocation = window.location;
+const setLocation = (location: string) => window.history.replaceState({}, '', location);
 
-afterAll(() => (window.location = originalLocation));
-
-const mockLocation = ({ search, pathname }: { search?: string; pathname?: string }) => {
-  // eslint-disable-next-line
-  delete (window as any).location;
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  window.location = Object.defineProperties(
-    {},
-    {
-      ...Object.getOwnPropertyDescriptors(originalLocation),
-      pathname: {
-        configurable: true,
-        value: pathname,
-      },
-      search: {
-        configurable: true,
-        value: search,
-      },
-      assign: {
-        configurable: true,
-        value: jest.fn(),
-      },
-    },
-  );
-};
+afterEach(() => setLocation('/'));
 
 const ShowLocale: React.FunctionComponent<{ setLocale: React.Dispatch<React.SetStateAction<string>> }> = () => (
   <main>{React.useContext(LocaleContext)}</main>
@@ -41,31 +18,68 @@ const renderWithLocale = () => render(<WithLocale>{(props) => <ShowLocale {...pr
 
 describe('default locale selection', () => {
   it('restores from browser state', () => {
-    mockLocation({ pathname: '/index.heb.html' });
+    setLocation('/index.heb.html');
     renderWithLocale();
 
     cleanup();
 
-    mockLocation({ pathname: '' });
+    setLocation('/');
     renderWithLocale();
 
     expect(screen.getByRole('main').textContent).toEqual('heb');
   });
 
   it('prioritizes lang parameter over url path', () => {
-    mockLocation({ search: '?lang=spa', pathname: '/index.heb.html' });
+    setLocation('/index.heb.html?lang=spa');
     renderWithLocale();
     expect(screen.getByRole('main').textContent).toEqual('spa');
   });
 
   it('prioritizes url path over browser storage', () => {
-    mockLocation({ pathname: '/index.heb.html' });
+    setLocation('/index.heb.html');
     renderWithLocale();
 
     cleanup();
 
-    mockLocation({ pathname: '/index.spa.html' });
+    setLocation('/index.spa.html');
     renderWithLocale();
     expect(screen.getByRole('main').textContent).toEqual('spa');
+  });
+
+  describe('fetching locale from APy', () => {
+    it('uses valid response', async () => {
+      renderWithLocale();
+
+      mockAxios.mockResponse({ data: ['es'] });
+      await waitFor(() =>
+        expect(mockAxios.post).toHaveBeenCalledWith(expect.stringContaining('/getLocale'), '', expect.anything()),
+      );
+
+      expect(screen.getByRole('main').textContent).toEqual('spa');
+    });
+
+    it('handles variants', () => {
+      renderWithLocale();
+
+      mockAxios.mockResponse({ data: ['en-US'] });
+
+      expect(screen.getByRole('main').textContent).toEqual('eng');
+    });
+
+    it('falls back to default for unknown response', () => {
+      renderWithLocale();
+
+      mockAxios.mockResponse({ data: ['zzz'] });
+
+      expect(screen.getByRole('main').textContent).toEqual(Config.defaultLocale);
+    });
+
+    it('falls back to default on failure', () => {
+      renderWithLocale();
+
+      mockAxios.mockError();
+
+      expect(screen.getByRole('main').textContent).toEqual(Config.defaultLocale);
+    });
   });
 });
