a95a3aba8881ed9da39d281a5cc2bec79931e303
vykliuk <alinavykliuk@gmail.com>
2025-07-15 02:18:03 -0400

    german pos

--- src/util/posLocalization.ts
+++ src/util/posLocalization.ts
@@ -1,6 +1,7 @@
 import eng from '../strings/pos/eng.json';
 import ukr from '../strings/pos/ukr.json';
 import rus from '../strings/pos/rus.json';
+import deu from '../strings/pos/deu.json';
 
 type PosMap = Record<string, Record<string, string>>;
 
@@ -8,6 +9,7 @@ const posMap: PosMap = {
   eng,
   ukr,
   rus,
+  deu,
 };
 
 export function getPosTag(locale: string, tagKey: string): string {
